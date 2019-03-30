fd1b1f3d2d1c7e6ece995a30ab2d555ed73c069d
little-eight <295928126@qq.com>
2019-03-30 17:23:15 +0800

    秒杀系统三init

---
+++ springboot-seckill/springboot-seckill-three/src/main/java/bdbk/seckill/service/OrderService.java
@@ -0,0 +1,49 @@
+package bdbk.seckill.service;
+
+import bdbk.seckill.dao.OrderDao;
+import bdbk.seckill.domain.OrderInfo;
+import bdbk.seckill.domain.SeckillUser;
+import bdbk.seckill.domain.UserOrder;
+import bdbk.seckill.vo.GoodsVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+    OrderDao orderDao;
+
+	public UserOrder getUserOrderByUserIdGoodsId(long userId, long goodsId) {
+		return orderDao.getUserOrderByUserIdGoodsId(userId, goodsId);
+	}
+
+	public OrderInfo getOrderInfoByUserIdGoodsId(long userId, long goodsId) {
+		return orderDao.getOrderInfoByUserIdGoodsId(userId, goodsId);
+	}
+
+	@Transactional
+	public OrderInfo createOrder(SeckillUser user, GoodsVo goods) {
+		OrderInfo orderInfo = new OrderInfo();
+		orderInfo.setCreateDate(new Date());
+		orderInfo.setDeliveryAddrId(0L);
+		orderInfo.setGoodsCount(1);
+		orderInfo.setGoodsId(goods.getId());
+		orderInfo.setName(goods.getName());
+		orderInfo.setPrice(goods.getSeckillPrice());
+		orderInfo.setOrderChannel(1);
+		orderInfo.setStatus(0);
+		orderInfo.setUserId(user.getId());
+		long orderId = orderDao.insertOrderInfo(orderInfo);
+		UserOrder userOrder = new UserOrder();
+		userOrder.setGoodsId(goods.getId());
+		userOrder.setOrderId(orderId);
+		userOrder.setUserId(user.getId());
+		orderDao.insertUserOrder(userOrder);
+		return orderInfo;
+	}
+	
+}
