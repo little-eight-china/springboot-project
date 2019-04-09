4215b39b3ef5d09ab425119ab3f116c4ae393716
little-eight <295928126@qq.com>
2019-04-09 21:33:19 +0800

    秒杀四+rabbitmq init

---
+++ springboot-seckill/springboot-seckill-four/README.md
@@ -0,0 +1,29 @@
+# springboot秒杀系统———第一章 初建模型
+
+
+## 包架构
+
+```
+└─ sql 建表sql
+└─src
+    └─bdbk
+        └─seckill
+            ├─config 配置类
+            ├─constant 常量类
+            ├─controller 控制类
+            ├─dao 数据库类
+            ├─domain 实体类
+            ├─service 逻辑类
+            ├─util 工具类
+            ├─validator 校验类
+            ├─vo 前端vo
+```
+
+## 实现部分
+
+### 整合rabbitmq
+* 基础安装知识
+> https://little8.top/2018/08/09/springboot%E6%95%B4%E5%90%88RabbitMQ%EF%BC%88windows%E7%8E%AF%E5%A2%83%EF%BC%89/
+ 
+
+
---
+++ springboot-seckill/springboot-seckill-four/src/main/java/bdbk/seckill/rabbitmq/MQReceiver.java
@@ -0,0 +1,51 @@
+package bdbk.seckill.rabbitmq;
+
+import bdbk.seckill.domain.SeckillUser;
+import bdbk.seckill.domain.UserOrder;
+import bdbk.seckill.service.GoodsService;
+import bdbk.seckill.service.OrderService;
+import bdbk.seckill.service.SeckillService;
+import bdbk.seckill.vo.GoodsVo;
+import com.alibaba.fastjson.JSON;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MQReceiver {
+
+		private static Logger log = LoggerFactory.getLogger(MQReceiver.class);
+
+
+		@Autowired
+		private GoodsService goodsService;
+		
+		@Autowired
+		private OrderService orderService;
+		
+		@Autowired
+		private SeckillService seckillService;
+		
+		@RabbitListener(queues=MQConfig.SECKILL_QUEUE)
+		public void receive(String message) {
+			log.info("receive message:"+message);
+			SeckillMessage seckillMessage  = JSON.parseObject(message, SeckillMessage.class);
+			SeckillUser user = seckillMessage.getUser();
+			long goodsId = seckillMessage.getGoodsId();
+			
+			GoodsVo goods = goodsService.getGoodsVoByGoodsId(goodsId);
+	    	int stock = goods.getStock();
+	    	if(stock <= 0) {
+	    		return;
+	    	}
+	    	//判断是否已经秒杀到了
+	    	UserOrder order = orderService.getUserOrderByUserIdGoodsId(user.getId(), goodsId);
+	    	if(order != null) {
+	    		return;
+	    	}
+	    	//减库存 下订单 写入秒杀订单
+	    	seckillService.seckill(user, goods);
+		}
+}
